ac585ded1be98f944311a4e3675c4608e2622ed1
Matt Hersh <mattmhersh@gmail.com>
2017-02-18 20:40:11 +0000

    Updated http2'

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ var express = require('express');
 var shrinkRay = require('shrink-ray');
 var http2 = require('http2');
 
-
 var app = express();
 
-require('express-http2-workaround')({ express:express, http2:http2, app:app });
+//require('express-http2-workaround')({ express:express, http2:http2, app:app });
+
+var expressHTTP2Workaround = require('express-http2-workaround');
+
 
 app.disable('x-powered-by');
 app.set('port', (process.env.PORT || 5000));
 
 //app.use(express.static(__dirname + '/public'));
+
+app.use(expressHTTP2Workaround({ express:express, http2:http2 }));
 
 app.use(express.static(__dirname + '/public', {
         maxAge: 86400000,
